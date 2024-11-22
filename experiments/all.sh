67bcab924a418abce0b14b9e05a09e091488359f
Martin L. Putra <martinluttap@uchicago.edu>
2024-11-22 20:23:30 +0000

    Contention experiment driver

--- experiments/all.sh
+++ experiments/all.sh
@@ -2,10 +2,11 @@
 
 #POLICIES="base elasticcontainer"
 #APPS="bwa fastqc samtools_sort samtools_index gatk_baserecal_spark star trimmomatic"
-POLICIES="elasticcontainer"
-APPS="bwa"
+POLICIES="autothrottle elasticcontainer"
+APPS="samtools_sort samtools_index gatk_baserecal"
 for POLICY in $POLICIES; do
   for APP in $APPS; do
-    python3 driver/corr-by_policies.py --app ${APP} --policy ${POLICY}
+    # python3 driver/corr-by_policies.py --app ${APP} --policy ${POLICY}
+    python3 driver/contention.py --app ${APP} --policy ${POLICY}
   done
 done
